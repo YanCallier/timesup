32f182c9b1f341aecc297bd13ebdf5004aa38400
Yan Callier <yan.callier@gmail.com>
2020-04-30 18:26:26 +0200

    test

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+const MongoClient = require('mongodb').MongoClient;
+
+// const uri = 'mongodb://localhost:27017/';
+// const uri = "mongodb+srv://yanAdmin:DATE2naissance@cluster0-mjp15.mongodb.net/test?retryWrites=true";
+const uri = "mongodb+srv://yanAdmin:DATE2naissance@cluster0-hcaod.gcp.mongodb.net/test?retryWrites=true&w=majority";
+
+app.use("/", express.static(__dirname));
+
+app.get('/in', function (req, res) {
+
+
+    MongoClient.connect(uri, { useNewUrlParser: true }, function (err, client) {
+        if (err) console.log('conexion error : ' + err);
+        let collection = client.db('timesup').collection('games');
+        console.log(req.query.word);
+
+        collection.updateOne(
+            { "name": "game0" },
+            { $push: { words: req.query.word } }
+        )
+        client.close();
+        res.redirect('/')
+    });
+});
+
+app.use(function (req, res) { res.status(404).send('Cette page n\'existe pas') });
+const PORT = process.env.PORT || 8080;
+server.listen(PORT, function () {
+    console.log('ping');
+});
